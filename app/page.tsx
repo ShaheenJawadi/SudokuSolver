76289905cf7121b751cd07ef4b80df4b8764d6e5
shaheenj <jawadishaheen@gmail.com>
2024-02-02 11:50:33 +0100

    Add redux Provider page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client"
  import Board from "./components/board";
+import { Provider } from 'react-redux'
+import { store } from "./store";
 
 export default function Home() {
-  return (
-    <main className="main">
-      <Board/>
-  
-    </main>
+  return ( 
+    <Provider store={store}>
+      <main className="main">
+      
+        <Board/>
+    
+      </main>
+    </Provider>
   );
 }
