1a39c2df4cac9713ed981bc1a095484e4cb5a7c2
shaheenj <jawadishaheen@gmail.com>
2024-02-02 12:26:04 +0100

    Styling the highlighted cell with cursor

--- app/components/board/cell.tsx
+++ app/components/board/cell.tsx
@@ -1,7 +1,7 @@
 
-import { AppDispatch} from "@/app/store";
+import { AppDispatch, RootState } from "@/app/store";
 import { setCursor } from "@/app/store/sudoku";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
  
 type Props ={
     row:number ,
@@ -13,13 +13,15 @@ const Cell =(props : Props )=>{
     const { column, row, digit }= props ;
 
     const dispatch = useDispatch<AppDispatch>()
- 
+    const store = useSelector((state: RootState) => state.sudoku)
+
+    const isCursor = (column == store.cursorPosition.x && row == store.cursorPosition.y ) ?"cursor" :""
    
     const handelClick =()=>{
          dispatch(setCursor({x:column , y:row}))
     }
     return (
-        <div className="cell" onClick={()=>handelClick()}>
+        <div className={`cell ${isCursor}`} onClick={()=>handelClick()}>
             { digit!=0 && digit}
         </div>
     )
